b581211b00834ad24683873a7417d253620c7ebe
michelamar <mmarchini@stuy.edu>
2018-02-02 14:49:32 -0500

    woo

---
+++ canvas.js
@@ -0,0 +1,10 @@
+var c = document.getElementById("yes_we_canvas");
+var cxt = c.getContext("2d");
+
+var b1 = document.getElementById("clear");
+var b2 = document.getElementById("toggle");
+
+var clear = function()
+
+b1.addEventListener("click", clear)
+b2.addEventListener("click", toggle)
